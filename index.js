108a2b740d26ea97a1e7ea4db0a3ca953b3ca199
linus-rudbeck <linus.rudbeck@distansakademin.se>
2023-06-02 06:16:18 +0200

    Create recipes working

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const mongodb = require('mongodb')
 const express = require('express')
-const exphbs =require('express-handlebars')
+const exphbs = require('express-handlebars')
+
+// Body parser for getting form data
+const bodyParser = require('body-parser')
 
 const app = express()
+
+// Set up body parser for form data
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.engine('hbs', exphbs.engine({
     extname: '.hbs',
@@ -11,35 +17,43 @@ app.engine('hbs', exphbs.engine({
 
 app.set('view engine', 'hbs')
 
+// Get all recipes    
 app.get('/recipes', async (req, res) => {
     const client = new mongodb.MongoClient("mongodb://127.0.0.1")
     await client.connect()
 
     const db = client.db("MyRecipeDB")
 
+    // Get all recipes
     const dbRecipes = db.collection("Recipes").find()
-    const recipes = []
-   
+    const recipes = await dbRecipes.toArray()
 
-    await dbRecipes.forEach(r => {
-     recipes.push(r)
-    })
-
-    res.render('home', {recipes})
+    res.render('home', { recipes })
 })
 
-app.post('/recipes/:id', async (req, res) =>{
-    const _id = new mongodb.ObjectId(req.params.id)
-    const client = new mongodb.MongoClient("mongodb://127.0.0.1:27017")
-     client.connect()
+// Show form to add new recipe
+app.get('/recipes/new', async (req, res) => {
+    res.render('new')
+})
 
+// Save new recipe
+app.post('/recipes/new', async (req, res) => {
+
+    const recipe = {
+        Title: req.body.title,
+        Summary: req.body.summary,
+        Minutes: req.body.minutes,
+    }
+    
+    const client = new mongodb.MongoClient("mongodb://127.0.0.1:27017")
+    client.connect()
     const db = client.db("MyRecipeDB")
 
-    db.collection("Recipes").findOneAndUpdate({_id: _id}, (err, recipe) => {
-       res.render('recipe', {recipe})
-    
-    })
+    await db.collection("Recipes").insertOne(recipe);
+
+    res.redirect('/recipes')
 })
+
 app.listen(8000, () => {
     console.log("http://127.0.0.1:8000/");
 })
